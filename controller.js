66b62e2e8763867afc5f3203e5f469e9c406001e
bryancardoso <bryancardoso42@gmail.com>
2023-07-07 08:53:59 -0300

    criei lista, variavel para tabela e juntei os dados com tabela

--- controller.js
+++ controller.js
@@ -19,6 +19,7 @@ function salvarUser(){
 
     if(nomeUser){
         dadosLista.push(nomeUser);
+        criaLista();
         // console.log(dadosLista);
     }
 }
@@ -26,6 +27,7 @@ function criaLista(){
     let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
     
     for(let i=0; i <= (dadosLista.length-1); i++){
-        tabela += "<tr><td>" + dadosLista[i] + "<td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger' onclick=''>Excluir</button>"
+        tabela += "<tr><td>" + dadosLista[i] + "<td><td><button class='btn btn-success' onclick=''>Editar</button><button class='btn btn-danger' onclick=''>Excluir</button><td><tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
