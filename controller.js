0f528f1d989e0fee9dddd471671c07f14cbc4ab9
bryancardoso <bryancardoso42@gmail.com>
2023-07-26 09:35:29 -0300

    colocamos a função focus no js

--- controller.js
+++ controller.js
@@ -22,20 +22,23 @@ function salvarUser(){
         criaLista();
         document.getElementById("nomeUser").value = '';
         // console.log(dadosLista);
+    }else{
+        alert("Digite um nome");
+        document.getElementById('nomeUser').focus()
     }
 }
 function criaLista(){
     let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
     
     for(let i=0; i <= (dadosLista.length-1); i++){
-        tabela += "<tr><td>" + dadosLista[i] + "<td><td><button class='btn btn-primary' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button><td><tr>";
+        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-primary' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
         document.getElementById('tabela').innerHTML = tabela;
     }
 }
 
 // FUNCAO PARA EDIÇÃO DE NOME
 
-function Editar(i){
+function editar(i){
     document.getElementById("nomeUser").value = dadosLista[(i - 1)];
     dadosLista.splice(dadosLista[(i-1), 1]);
 }
